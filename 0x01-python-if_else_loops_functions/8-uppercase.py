b90c468baef1fc2502e25a404d971af7eb259337
Amr Adel <amr96.adel@gmail.com>
2023-10-04 01:24:46 +0300

    commit 4-2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) in range(65, 91):
+        if ord('c') in range(65, 91):
             print("{}".format(c), end='')
         elif ord(c) in range(97, 123):
-            print("{}".format(chr(ord(c) + 32)), end='')
+            print("{}".format(chr(ord('c') + 32)), end='')
