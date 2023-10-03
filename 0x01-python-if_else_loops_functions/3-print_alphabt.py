5373df64b7ed85b6ac279a7001bd9f9a4615b561
Amr Adel <amr96.adel@gmail.com>
2023-10-04 00:51:42 +0300

    commit 2-1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == "q" or i == "e":
+    if chr(i) == "q" or chr(i) == "e":
         continue
-    print(chr(i), end=' ')
+    print(f"{chr(i)}",end='')
