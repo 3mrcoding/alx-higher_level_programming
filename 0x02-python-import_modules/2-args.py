7f9c1ae28676e41096cc9d4b1561bf123269ab0e
Amr Adel <amr96.adel@gmail.com>
2023-10-06 23:49:10 +0300

    task-3-3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
-
-n = len(sys.argv)
-if n == 2:
-    print("{} argument:".format(n-1))
-    print("1: {}".format(sys.argv[1]))
-elif n == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(n-1))
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i]))
+    n = len(sys.argv)
+    if n == 2:
+        print("{} argument:".format(n-1))
+        print("1: {}".format(sys.argv[1]))
+    elif n == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
