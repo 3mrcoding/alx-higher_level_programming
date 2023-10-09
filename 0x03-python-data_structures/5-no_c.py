9862ae7bfdbd0696c12d59f675b79972952b9267
Amr Adel <amr96.adel@gmail.com>
2023-10-09 22:44:30 +0300

    task6-2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    for i in range(0, len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            my_string[i] = []
-    return my_string
+
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string
