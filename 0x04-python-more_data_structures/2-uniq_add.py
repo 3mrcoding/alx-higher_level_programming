3427ca562e9c63b961b8370c1326fc31ff9899b9
Amr Adel <amr96.adel@gmail.com>
2023-10-12 00:04:32 +0300

    task 2-1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    
+    y = 0
+    x = my_list.copy()
+    x = list(set(x))
+    print(x)
+    for i in range(len(x)):
+        y += x[i]
+    return y
