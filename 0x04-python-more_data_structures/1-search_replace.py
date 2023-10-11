0b469937c0004e61e4abb785668ad137838fd9c2
Amr Adel <amr96.adel@gmail.com>
2023-10-11 23:49:38 +0300

    task 1-5

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
     x = my_list.copy()
-
     for i in range(len(x)):
-        if(x[i] == search):
+        if x[i] == search:
             x[i] = replace
-
     return x
