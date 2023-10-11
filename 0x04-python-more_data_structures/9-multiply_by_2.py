12c6c90a9e8e03b47a7f29771fdd00e0ce32ca3e
Amr Adel <amr96.adel@gmail.com>
2023-10-12 01:23:01 +0300

    task 10-2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for value in a_dictionary:
-        a_dictionary[value] *= 2
-    return a_dictionary
+    x = a_dictionary.copy
+    for value in x:
+        x[value] *= 2
+    return x
