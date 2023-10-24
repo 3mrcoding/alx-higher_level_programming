af53e3492ed0b42421baebff6e48a66eed89bb97
Amr Adel <amr96.adel@gmail.com>
2023-10-25 00:26:34 +0300

    TASK 01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Square class to define size of square"""
+
+
+class Square:
+    """Class representing a square"""
+    
+    
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
