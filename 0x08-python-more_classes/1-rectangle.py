2e5f827f1f5d8299721a0adb7acd3677a4ee5af6
AMR ADEL <amr96.adel@gmail.com>
2023-10-30 21:47:26 +0200

    TASK 1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""python class file"""
+
+
+class Rectangle:
+    """class of a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getter method of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter method of width"""
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter method of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter method for height"""
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            self.__height = value
