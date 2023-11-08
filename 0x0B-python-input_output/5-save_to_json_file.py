ac2bab37cf23c9eeaf1c4393fea326ab6da28fc6
AMR ADEL <amr96.adel@gmail.com>
2023-11-08 22:01:40 +0200

    TASK 06

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a file that contain JSON string function"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    """a function that writes an Object to a text file, using a JSON representation"""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(json.dumps(my_obj))
