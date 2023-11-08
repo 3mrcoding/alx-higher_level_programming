2d8ca1516a09a690423b88a845ccde64d0a9282f
AMR ADEL <amr96.adel@gmail.com>
2023-11-08 21:38:22 +0200

    TASK 00-01

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-"""function file to read a file in the working dir"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
 
-    """function reads a file"""
+    """Print the contents of a UTF8 text file to stdout."""
 
-    with open(filename, "r", encoding="UTF8") as file:
-        print(file.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
