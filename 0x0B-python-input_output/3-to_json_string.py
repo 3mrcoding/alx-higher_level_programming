20aa2d5438173ad7bc2e7d74d6093c7f7f8219a8
AMR ADEL <amr96.adel@gmail.com>
2023-11-08 21:50:11 +0200

    TASK 04-02

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,12 +2,12 @@
 
 """Define a file that contain JSON string function"""
 
+
 import json
+
 
 def to_json_string(my_obj):
 
-
     """function returns the JSON representation of an object (string)"""
-
 
     return json.dumps(my_obj)
