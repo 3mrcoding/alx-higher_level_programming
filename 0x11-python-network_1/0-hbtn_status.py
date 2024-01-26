b7f2dbf9dbfcdaa8e7d76887bdc22c65e5eac8bb
Amr Adel <amr96.adel@gmail.com>
2024-01-26 21:59:35 +0200

    TASK 00

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib
+from urllib.request import urlopen
+
+url = "https://alx-intranet.hbtn.io/status"
+with urlopen(url) as response:
+    body = response.read()
+
+print(f"Body response:\n\t - type: {type(body)}\n\t - content: {body}\n\t - utf8 content: {body.decode('UTF-8')}")
+
