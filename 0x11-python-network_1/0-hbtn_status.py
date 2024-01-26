d48f0db3e32015a6886b4e596437635cd1bcba6c
Amr Adel <amr96.adel@gmail.com>
2024-01-26 22:03:59 +0200

    TASK 00 - 01

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,6 @@ url = "https://alx-intranet.hbtn.io/status"
 with urlopen(url) as response:
     body = response.read()
 
-print(f"Body response:\n\t - type: {type(body)}\n\t - content: {body}\n\t - utf8 content: {body.decode('UTF-8')}")
-
+print(
+    f"Body response: \n\t - type: {type(body)}\n\t - content: {body}\n\t"
+    f" - utf8 content: {body.decode('UTF-8')}")
