665fe2334ee5f78f5741e0dd49303f376e4fcfe1
Amr Adel <amr96.adel@gmail.com>
2024-01-26 22:04:53 +0200

    TASK 00 -02

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,5 @@ with urlopen(url) as response:
     body = response.read()
 
 print(
-    f"Body response: \n\t - type: {type(body)}\n\t - content: {body}\n\t"
+    f"Body response:\n\t - type: {type(body)}\n\t - content: {body}\n\t"
     f" - utf8 content: {body.decode('UTF-8')}")
