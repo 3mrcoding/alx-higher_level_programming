90d370801a207db081b82fbd29e07e0c312a31c5
Amr Adel <amr96.adel@gmail.com>
2024-01-27 11:14:03 +0200

    TASK 06 - 02

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,13 +3,11 @@
 to the passed URL with the email as a parameter, and finally displays
 the body of the response.
 """
-
 import requests
 import sys
 
-url = sys.argv[1]
-data = {'email': sys.argv[2]}
-
 if __name__ == '__main__':
-    r = requests.post(url, data=data)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
     print(r.text)
