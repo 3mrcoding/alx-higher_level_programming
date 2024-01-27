b43273ddde2bb2c5bac408fb048eab332adb093e
Amr Adel <amr96.adel@gmail.com>
2024-01-27 11:13:23 +0200

    TASK 06 - 01

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response."""
+"""script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response.
+"""
 
 import requests
 import sys
